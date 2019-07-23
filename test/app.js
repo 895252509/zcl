516150d4c1d1fa3b25beb447f566167e39404232
zhouxiang <895252509@qq.com>
2019-07-23 23:10:29 +0900

    DOC ADD

--- test/app.js
+++ test/app.js
@@ -40,7 +40,7 @@ zcl.on("timing$asyn",(e)=>{
   document.querySelector("#date").innerHTML = 
     `当前时间: ${date.getFullYear()}-${date.getMonth()+1}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   document.querySelectorAll("#fps")[0].innerHTML = `FPS:${e.timing.fps}`;
-  document.querySelectorAll("#avemillisecond")[0].innerHTML = `平均毫秒数:${e.timing.avemillisecond}`;
+  document.querySelectorAll("#avemillisecond")[0].innerHTML = `平均毫秒数:${e.timing.avemillisecond}ms`;
 });
 
 console.table(EventNames);
