fa5dade27db9b6abc5119b06e7970aeaa5689313
895252509 <895252509qq.com>
2019-08-05 16:17:24 +0800

    eslint added

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 let zcl = new Zcl("#can");
 let re = new Rectangle(new Shapes.point(20, 20), new Shapes.point(100, 100));
-re.on("click", function (e) {
-  console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
+re.on("click", function(e) {
+    console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
 })
 
 re.on("mousemove", (e) => {
@@ -10,12 +10,12 @@ re.on("mousemove", (e) => {
 
 let c = new Circle(new Shapes.point(100, 300), 100);
 c.on("click", (e) => {
-  console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
+    console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
 })
 
 let l3 = new Line(new Shapes.point(100, 100), new Shapes.point(260, 200));
 l3.on("mousemove", (e) => {
-  console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
+    console.log(`${e.type}:${e.offsetX},${e.offsetY}`);
 })
 
 zcl.add(re);
@@ -23,23 +23,23 @@ zcl.add(c);
 
 zcl.add(l3);
 let po1 = new Polygon(l3.getBoundingBox());
-po1.on("click", function (e) {
-  console.log(`po1:${e.type}:${e.offsetX},${e.offsetY}`);
+po1.on("click", function(e) {
+    console.log(`po1:${e.type}:${e.offsetX},${e.offsetY}`);
 });
 zcl.add(po1);
 
 zcl.start();
 
 zcl.on("click", (e) => {
-  console.log(`po1:${e.type}:${e.offsetX},${e.offsetY}`);
+    console.log(`po1:${e.type}:${e.offsetX},${e.offsetY}`);
 });
 
 zcl.on("timing$asyn", (e) => {
-  let date = new Date(e.timing.framesecond * 1000);
-  document.querySelector("#date").innerHTML =
-    `当前时间: ${date.getFullYear()}-${date.getMonth()+1}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  document.querySelectorAll("#fps")[0].innerHTML = `FPS:${e.timing.fps}`;
-  document.querySelectorAll("#avemillisecond")[0].innerHTML = `平均毫秒数:${e.timing.avemillisecond}ms`;
+    let date = new Date(e.timing.framesecond * 1000);
+    document.querySelector("#date").innerHTML =
+        `当前时间: ${date.getFullYear()}-${date.getMonth()+1}-${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    document.querySelectorAll("#fps")[0].innerHTML = `FPS:${e.timing.fps}`;
+    document.querySelectorAll("#avemillisecond")[0].innerHTML = `平均毫秒数:${e.timing.avemillisecond}ms`;
 });
 
 
--- zcl-lib/const.js
+++ zcl-lib/const.js
@@ -4,37 +4,20 @@
 
 // 事件名称
 // 鼠标事件
-const EventNamesMouse =
-  [
-    'click'
-    , 'contextmenu'
-    , 'mousemove'
-    , 'mousedown'
-    , 'mouseup'
-    , 'dbclick'
-    , 'wheel'
-    , 'mouseover'
-    , 'focus'
-    , 'blur'
-  ];
+const EventNamesMouse = [
+    'click', 'contextmenu', 'mousemove', 'mousedown', 'mouseup', 'dbclick', 'wheel', 'mouseover', 'focus', 'blur'
+];
 
 // 键盘事件
-const EventNamesKeywords =
-  [
-    'keydown'
-    , 'keypress'
-    , 'keyup'
-  ];
+const EventNamesKeywords = [
+    'keydown', 'keypress', 'keyup'
+];
 
 // 鼠标+键盘
 const EventNames =
-  EventNamesMouse.concat(EventNamesKeywords);
+    EventNamesMouse.concat(EventNamesKeywords);
 
 // 自定义事件
 const EventNamesZcl = [
-  'beforeframe'
-  , 'afterframe'
-  , 'beforeinit'
-  , 'afterinit'
-  , 'timing'
-]
+    'beforeframe', 'afterframe', 'beforeinit', 'afterinit', 'timing'
+];
