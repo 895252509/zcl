de4f0230be1e3cc76e58174f90045aafae3b4d5c
895252509 <895252509@qq.com>
2020-01-24 16:59:56 +0800

    格式化代码

--- zcl-lib/core/const.js
+++ zcl-lib/core/const.js
@@ -5,29 +5,29 @@
 // 事件名称
 // 鼠标事件
 const EventNamesMouse = [
-    'click', 
-    'contextmenu', 
-    'mousemove', 
-    'mousedown', 
-    'mouseup', 
-    'dbclick', 
-    'wheel', 
-    'mouseover', 
-    'focus', 
-    'blur',
-    'mouseout'
+  'click',
+  'contextmenu',
+  'mousemove',
+  'mousedown',
+  'mouseup',
+  'dbclick',
+  'wheel',
+  'mouseover',
+  'focus',
+  'blur',
+  'mouseout'
 ];
 
 // 键盘事件
 const EventNamesKeywords = [
-    'keydown', 'keypress', 'keyup'
+  'keydown', 'keypress', 'keyup'
 ];
 
 // 鼠标+键盘
 const EventNames =
-    EventNamesMouse.concat(EventNamesKeywords);
+  EventNamesMouse.concat(EventNamesKeywords);
 
 // 自定义事件
 const EventNamesZcl = [
-    'beforeframe', 'afterframe', 'beforeinit', 'afterinit', 'timing', 'focus', 'blur'
+  'beforeframe', 'afterframe', 'beforeinit', 'afterinit', 'timing', 'focus', 'blur'
 ];
