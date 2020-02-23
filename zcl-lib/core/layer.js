511e037267eecb8657ba44d886d09317934b9d9b
ZX <895252509@qq.com>
2020-02-23 21:17:34 +0800

    layer up

--- zcl-lib/core/layer.js
+++ zcl-lib/core/layer.js
@@ -6,29 +6,39 @@
  * 
  */
 class ZcLayers extends Eventable{
-  constructor( zcl, { 
-    usemulitdom
+  constructor( str, { 
+    usemulitdom,
+    layered
    } ){
     super();
 
     this._usermulitdom = usemulitdom || true;
-
-    // zcl
-    this.zcl = zcl;
+    this._layered = layered || true;
 
     // 原始的画布
-    this._cvs = zcl.cvs;
+    this._cvs = null; // zcl.cvs;
 
     // 层管理数组
     this._layers = [];
 
-    // 变换矩阵
+    // 每次缩放的缩放比率
+    this._scaleRate = 0.02;
+    // 浏览器坐标到canvas坐标的变换
     this._transformTo = new Matrix32();
 
-    this._createLayer();
+    // 鼠标点击画布
+    this._isclick = false;
 
-    this._background = this.create(-1);
+    // 上一次鼠标位置
+    this._preoffsetX = 0;
+    this._preoffsetY = 0;
 
+    this._init(str);
+
+    //this._createLayer();
+
+    //this._background = this.create(-1);
+/*
     this._background.draw = function(color = 'rgba(40, 120, 255, 1)'){
       var icvs = this.ctx;
 
@@ -81,27 +91,28 @@ class ZcLayers extends Eventable{
 
       icvs.restore();
     }
-
+*/
   }
 
   add(m){
     if(! m instanceof Displayable ) return;
-    this._layers[1].addChild(m);
+    this._layers[0].addChild(m);
   }
 
   show(){
-    this._cvs.clearRect(0, 0, this.width, this.width);
+    this._ctx.clearRect(0, 0, this.width, this.width);
 
     for (const lay of this._layers) {
       lay.show();
       if( !this._usermulitdom ){
-        this._cvs.drawImage(lay.dom, 0, 0);
+        this._ctx.drawImage(lay.dom, 0, 0);
       }
     }
   }
 
   /**
    * 基于原始画布创建一个相同大小的分层画布
+   * 创建一个画布，并且添加到子对象
    * 
    */
   _createLayer(zindex){
@@ -144,8 +155,10 @@ class ZcLayers extends Eventable{
       laydom.style.left = '0';
       laydom.style.padding = '0';
       laydom.style.margin = '0';
-      this.zcl.container.appendChild(laydom);
-    }else{
+      this.container.appendChild(laydom);
+    }
+    // 如果不使用dom分层，那么就暂时添加到body里，开发的时候用的
+    else{
       document.body.appendChild(laydom);
     }
 
@@ -158,19 +171,222 @@ class ZcLayers extends Eventable{
     return lay;
   }
 
+  /**
+   * 鼠标移动事件
+   * @param {Event} e 
+   */
   onmousemove(e){
-    let cp = new Shapes.point(e._worldX, e._worldY);
+    // 计算坐标变换
+    let p = new S.point(e.offsetX, e.offsetY);
+    p.dotMatrix(this._transformTo.invert());
+    const _worldX = p._x;
+    const _worldY = p._y;
+    const _movedX = e.offsetX - this._preoffsetX;
+    const _movedY = e.offsetY - this._preoffsetY;
+
+    // 判断是否划过对象，并设置鼠标样式
+    let cp = new Shapes.point(_worldX, _worldY);
     for (const lay of this.childen) {
       for (const m of lay.childen) {
         if( m.contain(cp) ){
-          this.zcl.candom.style.cursor = "pointer";
+          this._cvs.style.cursor = "pointer";
           this._hover = m;
           return;
         }
       }
     }
-    this.zcl.candom.style.cursor = "auto";
+    if( this._cvs.style.cursor != "pointer" )
+      this._cvs.style.cursor = "auto";
     this._hover = null;
+
+    // 拖动画布操作处理
+    if( this._isclick ){
+      const p = new S.point(_movedX, _movedY);
+      if( p._x !== 0 || p._y !== 0 ){
+        this._transformTo.translate(p._x, p._y);
+        this._cvs.style.cursor = "pointer";
+        this.trigger("transform", this._transformTo);
+      }
+    }
+    this._preoffsetX = e.offsetX;
+    this._preoffsetY = e.offsetY;
+  }
+
+  _init(dom){
+    if( typeof dom !== 'string' && !(dom instanceof HTMLElement)){
+      throw new Error('init error');
+    }
+    const tdom = dom instanceof HTMLElement? dom : document.querySelector(dom);
+    if( tdom.nodeName.toUpperCase() === 'CANVAS' ){
+      this.container = tdom.parentNode;
+      this._cvs = tdom;
+      this._ctx = this._cvs.getContext('2d');
+      // 如果给定的dom是一个canvas，那么就不使用分层dom。
+      this._layer_usemulitdom = false;
+    }else{
+      // 如果给定的dom不是一个canvas，通常是一个div容器，那么就创建一个画布dom添加进去
+      this.container = tdom;
+      const firstLayer = this._createLayer();
+      // 把这个画布放在最上面，用来接收dom消息
+      firstLayer.zIndex = 1000;
+      firstLayer.width = tdom.clientWidth;
+      firstLayer.height = tdom.clientHeight;
+      this._cvs = firstLayer.dom;
+      this._ctx = firstLayer.ctx;
+    }
+
+    this._dispatchEvent();
+  }
+
+  /**
+   * 分发dom事件
+   */
+  _dispatchEvent(){
+    for (const eventname of EventNamesMouse) {
+      this._cvs.addEventListener(eventname, (e) => {
+        this.trigger(eventname, e);
+      });
+    }
+
+    for (const eventname of EventNamesKeywords) {
+      window.addEventListener(eventname, (e) => {
+        // TODO
+
+      });
+    }
+  }
+  
+  /**
+   * 处理事件对象
+   * 1. 给事件对象添加转换到画布坐标的鼠标活动坐标
+   * @override
+   * @param {Event} e
+   * @returns {Zcl} this 
+   */
+  additionEvent(e){
+    // 如果是鼠标事件
+    if( e instanceof MouseEvent ){
+      let p = new S.point(e.offsetX, e.offsetY);
+      p.dotMatrix(this._transformTo.invert());
+      e._worldX = p._x;
+      e._worldY = p._y;
+      e._movedX = e.offsetX - this._preoffsetX;
+      e._movedY = e.offsetY - this._preoffsetY;
+    }
+    return this;
+  }
+
+  /**
+   * 重置变换
+   */
+  resetTransform(){
+    this._transformTo.reset();
+    this.trigger("transform", this._transformTo);
+  }
+
+  /**
+   * 鼠标滚轮滚动事件
+   * @param {Event} e 
+   */
+  onwheel(e){
+    // 实现滑动滚轮缩放画面
+    const wheeld =  e.wheelDelta || e.deltaY;
+    const scale = wheeld < 0? 1-this._scaleRate : 1+this._scaleRate;
+
+    // 先计算浏览器坐标到画布坐标的变换矩阵
+    this._transformTo.scale(scale, scale);
+    // 缩放全局画布
+    this.trigger("transform", this._transformTo);
+  }
+
+  /**
+   * 鼠标左键按下事件
+   * @param {Event} e 
+   */
+  onmouseleftdown(e){
+    if( this._hover === null ){
+      this._isclick = true;
+    }
+  }
+
+  /**
+   * 鼠标左键按下事件
+   * @param {Event} e 
+   */
+  onmouseleftup(e){
+    this._isclick = false;
+    this._cvs.style.cursor = "auto";
+  }
+
+  /**
+   * 鼠标划出画布事件
+   * @param {Event} e 
+   */
+  onmouseout(e){
+    this._preoffsetX = 0;
+    this._preoffsetY = 0;
+    this._isclick = false;
+    this._cvs.style.cursor = "auto";
+  }
+
+  /**
+   * 阻止右键菜单
+   * @param {MouseEvent} e 
+   */
+  oncontextmenu(e){
+    if(e.preventDefault){
+      e.preventDefault();
+    }else{
+      window.event.returnValue == false;
+    }
+  }
+
+  /**
+   * 防止双击时选中文字
+   * @param {MouseEvent} e 
+   */
+  ondbclick(e){
+    if(e.preventDefault){
+      e.preventDefault();
+    }else{
+      window.event.returnValue == false;
+    }
+  }
+
+  /**
+   * 把变换矩阵应用到画布
+   */
+  ontransform(e){
+    const m = e;
+    this._ctx.setTransform( m[0],0,0,m[3],m[4],m[5] );
+  }
+
+  /**
+   * 重新定义事件，分别处理鼠标按下事件
+   * @param {Event} e 
+   */
+  onmousedown(e){
+    if( e.button === 0 ){
+      this.trigger('mouseleftdown', e);
+    }else if( e.button === 1 ){
+      this.trigger('mousemiddledown', e);
+    }else if( e.button === 2 ){
+      this.trigger('mouserightdown', e);
+    }
+  }
+
+  /**
+   * 重新定义事件，分别处理鼠标抬起事件
+   * @param {Event} e 
+   */
+  onmouseup(e){
+    if( e.button === 0 ){
+      this.trigger('mouseleftup', e);
+    }else if( e.button === 1 ){
+      this.trigger('mousemiddleup', e);
+    }else if( e.button === 2 ){
+      this.trigger('mouserightup', e);
+    }
   }
 
   /**
@@ -180,7 +396,7 @@ class ZcLayers extends Eventable{
    */
   get height(){
     if( this._cvs !== null ){
-      return this._cvs.canvas.height;
+      return this._cvs.height;
     }
   }
 
@@ -191,7 +407,7 @@ class ZcLayers extends Eventable{
    */
   get width(){
     if( this._cvs !== null ){
-      return this._cvs.canvas.width;
+      return this._cvs.width;
     }
   }
 
@@ -280,5 +496,13 @@ class ZcLayer extends Eventable{
 
   get width(){
     return this._dom.width;
+  }
+
+  get zIndex(){
+    return this._dom.style.zIndex;
+  }
+
+  set zIndex(x){
+    this._dom.style.zIndex = x;
   }
 }
--- zcl-lib/zcl.js
+++ zcl-lib/zcl.js
@@ -29,24 +29,13 @@ class Zcl extends Eventable {
     // 通过为每一层分别创建画布dom，设置绝对定位重叠来实现分层
     this._layer_usemulitdom = true;
 
-    this._initContainer(params);
+    // this._initContainer(params);
 
-    this.layerManager = new ZcLayers(this,{
-      usemulitdom: this._layer_usemulitdom
+    this.layerManager = new ZcLayers(params,{
+      usemulitdom: this._layer_usemulitdom,
+      layered: this._layered
     });
     this.addChild( this.layerManager );
-
-    // 每次缩放的缩放比率
-    this._scaleRate = 0.02;
-    // 浏览器坐标到canvas坐标的变换
-    this._transformTo = new Matrix32();
-
-    // 鼠标点击画布
-    this._isclick = false;
-
-    // 上一次鼠标位置
-    this._preoffsetX = 0;
-    this._preoffsetY = 0;
 
     // 初始化操作
     this.init();
@@ -56,16 +45,11 @@ class Zcl extends Eventable {
     if (!(m instanceof Displayable))
       return;
 
-    // this.models.add(m);
     this.layerManager.add(m);
   }
 
   init() {
-
     this.trigger( "beforeinit", this );
-
-    // 管理dom传递的事件
-    this._dispatchEvent();
 
     this.trigger( "afterinit", this );
   }
@@ -109,110 +93,10 @@ class Zcl extends Eventable {
   }
 
   /**
-   * 处理事件对象
-   * 1. 给事件对象添加转换到画布坐标的鼠标活动坐标
-   * @override
-   * @param {Event} e
-   * @returns {Zcl} this 
+   * 重置变换
    */
-  additionEvent(e){
-    // 如果是鼠标事件
-    if( e instanceof MouseEvent ){
-      let p = new S.point(e.offsetX, e.offsetY);
-      p.dotMatrix(this._transformTo.invert());
-      e._worldX = p._x;
-      e._worldY = p._y;
-      e._movedX = e.offsetX - this._preoffsetX;
-      e._movedY = e.offsetY - this._preoffsetY;
-    }
-    return this;
-  }
-
-  /**
-   * 鼠标滚轮滚动事件
-   * @param {Event} e 
-   */
-  onwheel(e){
-    // 实现滑动滚轮缩放画面
-    const wheeld =  e.wheelDelta || e.deltaY;
-    const scale = wheeld < 0? 1-this._scaleRate : 1+this._scaleRate;
-
-    // 先计算浏览器坐标到画布坐标的变换矩阵
-    this._transformTo.scale(scale, scale);
-    // 缩放全局画布
-    this.trigger("transform", this._transformTo);
-  }
-
-  /**
-   * 鼠标移动事件
-   * @param {Event} e 
-   */
-  onmousemove(e){
-    this.additionEvent(e);
-
-    if( this._isclick ){
-      const p = new S.point(e._movedX, e._movedY);
-      if( p._x !== 0 || p._y !== 0 ){
-        this._transformTo.translate(p._x, p._y);
-        this.trigger("transform", this._transformTo);
-      }
-    }
-    this._preoffsetX = e.offsetX;
-    this._preoffsetY = e.offsetY;
-
-
-  }
-
-  /**
-   * 鼠标左键按下事件
-   * @param {Event} e 
-   */
-  onmouseleftdown(e){
-    if( this.layerManager._hover === null ){
-      this._isclick = true;
-    }
-  }
-
-  /**
-   * 鼠标左键按下事件
-   * @param {Event} e 
-   */
-  onmouseleftup(e){
-    this._isclick = false;
-  }
-
-  /**
-   * 鼠标划出画布事件
-   * @param {Event} e 
-   */
-  onmouseout(e){
-    this._preoffsetX = 0;
-    this._preoffsetY = 0;
-    this._isclick = false;
-  }
-
-  /**
-   * 阻止右键菜单
-   * @param {MouseEvent} e 
-   */
-  oncontextmenu(e){
-    if(e.preventDefault){
-      e.preventDefault();
-    }else{
-      window.event.returnValue == false;
-    }
-  }
-
-  /**
-   * 防止双击时选中文字
-   * @param {MouseEvent} e 
-   */
-  ondbclick(e){
-    if(e.preventDefault){
-      e.preventDefault();
-    }else{
-      window.event.returnValue == false;
-    }
+  resetTransform(){
+    this.layerManager.resetTransform();
   }
 
   /**
@@ -223,231 +107,4 @@ class Zcl extends Eventable {
     return window.performance?window.performance.timing.navigationStart + window.performance.now():new Date() .getTime();
   }
 
-  /**
-   * 把变换矩阵应用到画布
-   */
-  ontransform(e){
-    const m = e;
-    this.cvs.setTransform( m[0],0,0,m[3],m[4],m[5] );
-  }
-
-  /**
-   * 重置变换
-   */
-  resetTransform(){
-    this._transformTo.reset();
-    this.trigger("transform", this._transformTo);
-  }
-
-  /**
-   * 临时函数-画一个背景图
-   * @param {string} color 
-   */
-  _clearScreen(color = 'rgba(40, 120, 255, 1)') {
-    var icvs = this.cvs;
-
-    icvs.save();
-    icvs.fillStyle = color;
-
-    // 计算应该清除的范围
-    const size = new S.point(this.candom.width, this.candom.height);
-    const pos = new S.point(0, 0);
-    pos.dotMatrix(this._transformTo.invert());
-    size.dotMatrix(this._transformTo.scaleM.invert());
-    icvs.fillRect(
-      pos._x,
-      pos._y,
-      size._x,
-      size._y);
-      
-    icvs.strokeStyle = "rgba(255, 255, 255, 1)";
-    icvs.lineWidth = 0.8;
-    icvs.setLineDash([6, 2, 6, 2]);
-    icvs.lineDashOffset = 2;
-
-    var pixSizeX = 25;
-    var pixSizeY = 25;
-    var numberX = this.candom.height / pixSizeX;
-    var numberY = this.candom.width / pixSizeY;
-
-    for (var i = 0; i <= numberX; i++) {
-      if (i % 4 == 0)
-        icvs.strokeStyle = "rgba(255, 255, 255, 0.9)";
-      else
-        icvs.strokeStyle = "rgba(255, 255, 255, 0.4)";
-      icvs.beginPath();
-      icvs.moveTo(0 + 0.5, i * pixSizeX + 0.5);
-      icvs.lineTo(this.candom.width + 0.5, i * pixSizeX + 0.5);
-      icvs.stroke();
-    }
-
-    for (var i = 0; i <= numberY; i++) {
-      if (i % 4 == 0)
-        icvs.strokeStyle = "rgba(255, 255, 255, 0.6)";
-      else
-        icvs.strokeStyle = "rgba(255, 255, 255, 0.4)";
-
-      icvs.beginPath();
-      icvs.moveTo(i * pixSizeY + 0.5, 0);
-      icvs.lineTo(i * pixSizeY + 0.5, this.candom.height + 0.5);
-      icvs.stroke();
-    }
-
-    icvs.restore();
-  }
-
-  /**
-   * 分发dom事件
-   */
-  _dispatchEvent(){
-    for (const eventname of EventNamesMouse) {
-      this.candom.addEventListener(eventname, (e) => {
-        this.trigger(eventname, e);
-      });
-    }
-
-    for (const eventname of EventNamesKeywords) {
-      window.addEventListener(eventname, (e) => {
-        // TODO
-
-      });
-    }
-  }
-
-  /**
-   * 初始化画布容器
-   * @param {string/HTMLElement} dom 
-   */
-  _initContainer( dom ){
-    if( typeof dom !== 'string' && !(dom instanceof HTMLElement)){
-      throw new Error('init error');
-    }
-    const tdom = dom instanceof HTMLElement? dom : document.querySelector(dom);
-    if( tdom.nodeName.toUpperCase() === 'CANVAS' ){
-      this.container = tdom.parentNode;
-      this.candom = tdom;
-      this.cvs = this.candom.getContext('2d',{ alpha : true });
-      // 如果给定的dom是一个canvas，那么就不使用分层dom。
-      this._layer_usemulitdom = false;
-    }else{
-      // 如果给定的dom不是一个canvas，通常是一个div容器，那么就创建一个画布dom添加进去
-      this.container = tdom;
-      this.candom = document.createElement("canvas");
-      this.container.appendChild(this.candom);
-      this.candom.width = tdom.clientWidth;
-      this.candom.height = tdom.clientHeight;
-      // 把这个画布放在最上面，用来接收dom消息
-      this.candom.style.zIndex = 1000;
-      this.cvs = this.candom.getContext('2d',{ alpha : true });
-      this.container.style.position = 'relative';
-      this.candom.style.position = 'absolute';
-      this.candom.style.top = '0';
-      this.candom.style.left = '0';
-      this.candom.style.padding = '0';
-      this.candom.style.margin = '0';
-    }
-  }
-
-  /**
-   * 重新定义事件，分别处理鼠标按下事件
-   * @param {Event} e 
-   */
-  onmousedown(e){
-    if( e.button === 0 ){
-      this.trigger('mouseleftdown', e);
-    }else if( e.button === 1 ){
-      this.trigger('mousemiddledown', e);
-    }else if( e.button === 2 ){
-      this.trigger('mouserightdown', e);
-    }
-  }
-
-  /**
-   * 重新定义事件，分别处理鼠标抬起事件
-   * @param {Event} e 
-   */
-  onmouseup(e){
-    if( e.button === 0 ){
-      this.trigger('mouseleftup', e);
-    }else if( e.button === 1 ){
-      this.trigger('mousemiddleup', e);
-    }else if( e.button === 2 ){
-      this.trigger('mouserightup', e);
-    }
-  }
-
-  /**
-   * @returns {HTMLCanvasElement}
-   */
-  get getCanvas(){
-    return this.candom;
-  }
-
-  /**
-   * @returns {OffscreenCanvasRenderingContext2D}
-   */
-  get pen(){
-    return this.cvs;
-  }
 }
-
-// /**
-//  * 对象管理类
-//  * 
-//  */
-// class Zclm extends Eventable {
-//   constructor(zcl) {
-//     super();
-
-//     this._models = [];
-
-//     this._zcl = zcl;
-
-//     this._focus = null;
-
-//     this._hover = null;
-//   }
-
-//   add(m) {
-
-//     if (!(m instanceof Displayable))
-//       return;
-
-//     this.addChild(m);
-    
-//     this._models.push(m);
-
-//   }
-
-//   /**
-//    * 响应鼠标移动事件
-//    *  
-//    * 1. 设置给模型对象设置鼠标划过效果
-//    * ~2. 判断鼠标划过状态，触发鼠标离开事件
-//    * 
-//    */
-//   onmousemove(e){
-//     let cp = new Shapes.point(e._worldX, e._worldY);
-//     let is = false;
-//     for (const m of this._models) {
-//       if (m.contain && m.contain(cp)) {
-//         this._zcl.candom.style.cursor = "pointer";
-//         this._hover = m;
-//         is = true;
-//         return;
-//       }
-//     }
-//     this._zcl.candom.style.cursor = "auto";
-//     this._hover = null;
-//   }
-
-//   lastedModel( m ){
-//     const moduls = this._models;
-//     const index = moduls.findIndex( v => v === m );
-//     if( index >= 0 && index < moduls.length - 1){
-//       this._models = moduls.filter( v => v !== m);
-//       this._models.push(m);
-//     }
-//   }
-// }
-
